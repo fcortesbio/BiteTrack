2cf69d353b382ae29bdc7876f86d29c1f137e2e2
andres <fcortesbio@gmail.com>
2025-09-11 01:40:00 -0500

    remove deprecated useNewUrlParser and useUnifiedTopology options from the MongoDB connection code

--- index.js
+++ index.js
@@ -46,10 +46,7 @@ app.use(
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
