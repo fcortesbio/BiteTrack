84bcf670baab69de71d22e8710f98ce82bec8693
andres <fcortesbio@gmail.com>
2025-09-11 00:57:15 -0500

    adding morgan middleware, adjust formatting for models

--- models/Customer.js
+++ models/Customer.js
@@ -1,43 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const customerSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true
+const customerSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      sparse: true, // Allows multiple null values but unique non-null values
+      lowercase: true,
+      trim: true,
+    },
+    lastTransaction: {
+      type: Date,
+      default: null,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true
+  {
+    timestamps: true,
   },
-  phoneNumber: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    sparse: true, // Allows multiple null values but unique non-null values
-    lowercase: true,
-    trim: true
-  },
-  lastTransaction: {
-    type: Date,
-    default: null
-  }
-}, {
-  timestamps: true
-});
+);
 
 // Transform output
-customerSchema.set('toJSON', {
-  transform: function(doc, ret) {
+customerSchema.set("toJSON", {
+  transform: function (doc, ret) {
     ret.id = ret._id;
     delete ret._id;
     delete ret.__v;
     return ret;
-  }
+  },
 });
 
-module.exports = mongoose.model('Customer', customerSchema);
+module.exports = mongoose.model("Customer", customerSchema);
--- models/Product.js
+++ models/Product.js
@@ -1,38 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  productName: {
-    type: String,
-    required: true,
-    trim: true
+const productSchema = new mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
   },
-  description: {
-    type: String,
-    trim: true
+  {
+    timestamps: true,
   },
-  count: {
-    type: Number,
-    required: true,
-    min: 0,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0
-  }
-}, {
-  timestamps: true
-});
+);
 
 // Transform output
-productSchema.set('toJSON', {
-  transform: function(doc, ret) {
+productSchema.set("toJSON", {
+  transform: function (doc, ret) {
     ret.id = ret._id;
     delete ret._id;
     delete ret.__v;
     return ret;
-  }
+  },
 });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
